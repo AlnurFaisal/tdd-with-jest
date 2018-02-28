b9ef9b2327631b5637f7d498a84b9f0a644557f7
Alnur_Faisal <alnurfaisal@outlook.com>
2018-02-28 14:10:17 +0800

    Test Commit 1

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -8,7 +8,19 @@ For a given natural number greater than zero return:
 */
 
 function fizzbuzz(number) {
-
+    var fizz = number / 3;
+    var buzz = number / 5;
+    fizz = fizz % 1;
+    buzz = buzz % 1;
+    if(fizz === 0){
+        return "fizz";
+    }
+    else if(buzz == 0){
+        return "buzz";
+    }
+    else{
+        return number;
+    }
 }
 
-module.exports = fizzbuzz
+module.exports = fizzbuzz;
